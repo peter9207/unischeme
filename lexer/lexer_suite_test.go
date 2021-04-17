6579e40ea64ed5c7fab3ecdc76cd5243a1dcc49d
Peter Zhang <peter920730@gmail.com>
2021-04-17 14:19:27 -0700

    added test

--- lexer/lexer_suite_test.go
+++ lexer/lexer_suite_test.go
@@ -48,6 +48,23 @@ var _ = Describe("programs with 1 expression", func() {
 			Ω(len(exp.FnCall.Parameters)).Should(Equal(1))
 			Ω(*exp.FnCall.Parameters[0].Int).Should(Equal(1))
 		})
+	})
+	Describe("function with 2 parameter", func() {
+		p, err := lexer.Parse("(foo 1 2)")
+		It("should not return errors", func() {
+			Ω(err).Should(BeNil())
+		})
+		It("should parse the correct amount of expressiosn", func() {
+			Ω(len(p.Expressions)).Should(Equal(1))
+		})
 
+		It("Should parse the correct values", func() {
+			exp := p.Expressions[0]
+			Ω(exp.FnCall.Name).Should(Equal("foo"))
+
+			Ω(len(exp.FnCall.Parameters)).Should(Equal(2))
+			Ω(*exp.FnCall.Parameters[0].Int).Should(Equal(1))
+			Ω(*exp.FnCall.Parameters[1].Int).Should(Equal(2))
+		})
 	})
 })
